e6c2a28ee7763e7d31cb5ee029c37442f5d5179f
Johannes Elferich <jojotux123@hotmail.com>
2025-04-05 14:17:16 -0400

    Initial experiments

--- src/torch_peaks/find_peaks.py
+++ src/torch_peaks/find_peaks.py
@@ -1,5 +1,22 @@
 import torch
+import torch.nn.functional as F
 
-def peak_local_max(
+def peak_local_max_2d(
         image: torch.Tensor,
-)
+        min_distance: int = 1,
+        threshold_abs: float = 0.0,
+        exclude_border: bool = True,
+):
+    return F.max_pool2d(
+        image.unsqueeze(0).unsqueeze(0),
+        kernel_size=(min_distance * 2 + 1, min_distance * 2 + 1),
+        stride=1,
+        padding=min_distance,
+    ).squeeze(0).squeeze(0) 
+
+if __name__ == "__main__":
+    # Example usage
+    image = torch.randn( 5, 5)
+    print(image)
+    peaks = peak_local_max_2d(image, min_distance=2, threshold_abs=0.5)
+    print(peaks)
