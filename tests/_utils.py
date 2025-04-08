31bc5b06fc94bd03013ffcae7794c78a436fe970
Johannes Elferich <jojotux123@hotmail.com>
2025-04-07 22:37:40 -0400

    3D peak finding

--- tests/_utils.py
+++ tests/_utils.py
@@ -2,7 +2,7 @@ import torch
 from torch_grid_utils import coordinate_grid
 from torch_grid_utils.fftfreq_grid import dft_center
 
-from torch_find_peaks.gaussians import Gaussian2D
+from torch_find_peaks.gaussians import Gaussian2D, Gaussian3D
 
 def create_test_image(size:int=100, peaks: torch.tensor = torch.tensor([]), noise_level=0.1):
     """
@@ -14,9 +14,7 @@ def create_test_image(size:int=100, peaks: torch.tensor = torch.tensor([]), nois
         noise_level: Level of noise to add
         
     Returns:
-        Tuple containing:
         - Image tensor with Gaussian peaks
-        - List of true peak parameters (amplitude, x0, y0, sigma_x, sigma_y)
     """
     # Create a blank image
     image = torch.randn((size, size)) * noise_level
@@ -37,3 +35,35 @@ def create_test_image(size:int=100, peaks: torch.tensor = torch.tensor([]), nois
 
     return image
 
+def create_test_volume(size:int=100, peaks: torch.tensor = torch.tensor([]), noise_level=0.1):
+    """
+    Create a test volume with known Gaussian peaks.
+    
+    Args:
+        size: Size of the square image
+        peaks: (n,7) tensor of peak parameters (amplitude, center_x, center_y, center_z, sigma_x, sigma_y, sigma_z)
+        noise_level: Level of noise to add
+        
+    Returns:
+        - Image tensor with Gaussian peaks
+    """
+    # Create a blank image
+    image = torch.randn((size, size, size)) * noise_level
+        
+    gaussian_model = Gaussian3D(
+        amplitude=peaks[:, 0],
+        center_x=peaks[:, 1],
+        center_y=peaks[:, 2],
+        center_z=peaks[:, 3],
+        sigma_x=peaks[:, 4],
+        sigma_y=peaks[:, 5],
+        sigma_z=peaks[:, 6],
+    )
+    
+    center = dft_center((size,size,size),rfft=False,fftshifted=True)
+    grid = coordinate_grid((size,size,size),center=center)
+
+    # Add Gaussian peaks to the image
+    image += gaussian_model(grid).sum(dim=0)
+
+    return image
--- tests/test_find_peaks.py
+++ tests/test_find_peaks.py
@@ -1,9 +1,9 @@
 import torch
 from torch_grid_utils import coordinate_grid
-from torch_find_peaks.find_peaks import find_peaks_2d
+from torch_find_peaks.find_peaks import find_peaks_2d, find_peaks_3d
 from torch_grid_utils.fftfreq_grid import dft_center
 
-from _utils import create_test_image
+from _utils import create_test_image, create_test_volume
 
 def test_peak_picking_2d():
     peaks = torch.tensor([[1, 0, 0, 5, 5], [1, -20, -20, 2, 2]], dtype=torch.float32)
@@ -18,3 +18,16 @@ def test_peak_picking_2d():
     assert torch.allclose(peak_detections[0], torch.tensor([30, 30]), atol=1.5)
     assert torch.allclose(peak_detections[1], torch.tensor([50, 50]), atol=1.5)
 
+def test_peak_picking_3d():
+    peaks = torch.tensor([[1, 0, 0, 0, 2, 2, 2], [1, -20, -20, -20, 2, 2, 2]], dtype=torch.float32)
+    data = create_test_volume(size=100, peaks=peaks, noise_level=0.05)
+    
+    # Small distance and low threshold should pick extra peaks
+    peak_detections = find_peaks_3d(data, min_distance=1, threshold_abs=0.1)
+    assert peak_detections.shape[0] > 2
+    # Appropriate values should find exactly the two peaks
+    peak_detections = find_peaks_3d(data, min_distance=5, threshold_abs=0.5)
+    print(peak_detections)
+    assert peak_detections.shape[0] == 2
+    assert torch.allclose(peak_detections[0], torch.tensor([30, 30, 30]), atol=1.5)
+    assert torch.allclose(peak_detections[1], torch.tensor([50, 50, 50]), atol=1.5)
